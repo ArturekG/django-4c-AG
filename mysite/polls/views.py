dc539b0523b8bc3d93223b259e87c65799c82708
Artur <artur_galuszka@wp.pl>
2024-03-13 08:50:27 +0100

    corrected urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+def counter(request):
+    template = loader.get_template('counter.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
